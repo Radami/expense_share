a9f2b9a784ba2a085ae512c8ae334d2c816bbb94
Radu Patulescu <radu.patulescu@gmail.com>
2023-12-09 08:18:53 +0900

    Add unique_constraint on group membership

--- splittime/models.py
+++ splittime/models.py
@@ -1,14 +1,37 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+from django.db.models import UniqueConstraint
 
 # Create your models here.
+
+
 
 class Group(models.Model):
     creator = models.ForeignKey('auth.User', related_name='user_location',
                                 on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
+    creation_date = models.DateTimeField("creation date")
+
+    def __str__(self):
+        return self.name
+    
+    def was_created_recently(self):
+        return self.creation_date >= timezone.now() - datetime.timedelta(days=7)
 
 class GroupMembership(models.Model):
-    group = models.ForeignKey(Group, related_name='location', on_delete=models.CASCADE)
-    member = models.ForeignKey('auth.User', related_name='user_location', 
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=['group', 'member'], name = 'group_membership_uniqueness')]
+    
+    group = models.ForeignKey(Group, related_name='membership_group', on_delete=models.CASCADE)
+    member = models.ForeignKey('auth.User', related_name='membership_member', 
                                on_delete=models.CASCADE)
+    
+    
+    
+    def __str__(self):
+        return self.group.name + "-" + self.member.username
