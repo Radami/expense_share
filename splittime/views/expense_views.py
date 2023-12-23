80734d8bdbe5d84a4ffff1df31e7d43747bfb6c9
Radu Patulescu <radu.patulescu@gmail.com>
2023-12-24 08:29:12 +0900

    Fix linting error expense_views

--- splittime/views/expense_views.py
+++ splittime/views/expense_views.py
@@ -1,10 +1,11 @@
-from typing import Any
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views import generic
 
 from ..models import Group, GroupMembership, Expense
+
 
 class ExpenseDetailsView(generic.DetailView):
     model = Expense
@@ -19,23 +20,26 @@ def add_expense(request, group_id):
     expense.currency = request.POST["expense_currency"]
     expense.amount = request.POST["expense_amount"]
     expense.group = group
-    expense.save()    
-   
-    return HttpResponseRedirect(reverse("splittime:group_details", args=(group_id,)))
+    expense.save()
+    return HttpResponseRedirect(reverse("splittime:group_details",
+                                        args=(group_id,)))
+
 
 def delete_expense(request, pk):
     expense = get_object_or_404(Expense, pk=pk)
 
-    # Check if the user performing the delete is a member of the group, otherwise error
+    # Check if the user performing the delete is a member of the group, 
+    # otherwise error
     group = expense.group
     memberships = GroupMembership.objects.filter(group=group)
     found = False
     for gm in memberships:
         if request.user == gm.member:
             found = True
-    if found == False:
-        return PermissionError()
+    if found is False:
+        raise PermissionDenied()
 
     expense.delete()
 
-    return HttpResponseRedirect(reverse("splittime:group_details", args=(group.id,)))
+    return HttpResponseRedirect(reverse("splittime:group_details",
+                                        args=(group.id,)))
