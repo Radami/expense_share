713a15cdc4bdfb8e05aa0d0e7aea6bdff6dd0e2a
Radu Patulescu <radu.patulescu@gmail.com>
2023-12-28 06:59:19 +0900

    Add creation date to Expense model

--- splittime/models.py
+++ splittime/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.utils import timezone
 from django.db.models import UniqueConstraint
 
-# Create your models here.
 
 class Group(models.Model):
     creator = models.ForeignKey('auth.User', related_name='user_location',
@@ -15,30 +14,35 @@ class Group(models.Model):
 
     def __str__(self):
         return self.name
-    
+
     def was_created_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=365) <= self.creation_date <= now
+
 
 class GroupMembership(models.Model):
 
     class Meta:
         constraints = [
-            UniqueConstraint(fields=['group', 'member'], name = 'group_membership_uniqueness')]
-    
-    group = models.ForeignKey(Group, related_name='membership_group', on_delete=models.CASCADE)
-    member = models.ForeignKey('auth.User', related_name='membership_member', 
+            UniqueConstraint(fields=['group', 'member'],
+                             name='group_membership_uniqueness')]
+
+    group = models.ForeignKey(Group, related_name='membership_group',
+                              on_delete=models.CASCADE)
+    member = models.ForeignKey('auth.User', related_name='membership_member',
                                on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.group.name + "-" + self.member.username
-    
+
+
 class Expense(models.Model):
     name = models.CharField(max_length=20)
     currency = models.CharField(max_length=3)
     amount = models.FloatField(max_length=9)
-    group = models.ForeignKey(Group, related_name='expense_group', on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, related_name='expense_group',
+                              on_delete=models.CASCADE)
+    creation_date = models.DateTimeField("creation_date")
 
     def __str__(self):
         return self.name + " for " + str(self.amount) + " " + self.currency
-
