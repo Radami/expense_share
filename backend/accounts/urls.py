af97c3ec2ed100afb84a4301698656fea078e5e3
radu <radu.patulescu@gmail.com>
2024-09-01 17:48:02 +0900

    Replicated GroupIndexView tests into GroupIndexAPIView Tests

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+app_name = "users"
 urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain"),
     path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
