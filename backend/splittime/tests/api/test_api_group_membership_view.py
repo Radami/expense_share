f75deb0f8c563d554e01c1e9f345927491ab93f6
radu <radu.patulescu@gmail.com>
2024-11-06 07:39:46 +0900

    Add group delete members API tests

--- backend/splittime/tests/api/test_api_group_membership_view.py
+++ backend/splittime/tests/api/test_api_group_membership_view.py
@@ -17,20 +17,6 @@ class GroupMembershipAddAPITests(APITestCase):
         cls.group1 = GroupHelpers.create_group(creator=cls.user1)
         GroupHelpers.add_user_to_group(cls.group1, cls.user2)
 
-    def setUp(self):
-        self.assertEqual(
-            self.client.login(username=self.user1.username, password="glassonion123"),
-            True,
-        )
-        response = self.client.post(
-            reverse("users:token_obtain"),
-            {"username": self.user1.username, "password": "glassonion123"},
-        )
-        self.assertEqual(response.status_code, 200)
-        token = response.data.get("access")
-        self.assertIsNotNone(token)
-        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
-
     def login(self, user):
         self.assertEqual(
             self.client.login(username=user.username, password="glassonion123"),
@@ -129,3 +115,79 @@ class GroupMembershipAPILoginTests(APITestCase):
         data = {"user_id": self.user2.id, "group_id": self.group1.id}
         response = self.client.post(reverse("splittime:api_delete_group_member_view"), data=data)
         self.assertEqual(response.status_code, 401)
+
+
+class GroupMembershipDeleteViewTests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.creator = UserHelpers.create_user(user_name="user1")  # creator
+        cls.member = UserHelpers.create_user(user_name="user2")  # member
+        cls.outsider = UserHelpers.create_user(user_name="user3")  # outsider
+
+    def setUp(self):
+        self.group1 = GroupHelpers.create_group(creator=self.creator)
+        GroupHelpers.add_user_to_group(self.group1, self.member)
+
+    def login(self, user):
+        self.assertEqual(
+            self.client.login(username=user.username, password="glassonion123"),
+            True,
+        )
+        response = self.client.post(
+            reverse("users:token_obtain"),
+            {"username": user.username, "password": "glassonion123"},
+        )
+        self.assertEqual(response.status_code, 200)
+        token = response.data.get("access")
+        self.assertIsNotNone(token)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+
+    def test_delete_group_member_as_creator(self):
+        # login as creator
+        self.login(self.creator)
+
+        # test begins
+        data = {"user_id": self.member.id, "group_id": self.group1.id}
+        response = self.client.post(reverse("splittime:api_delete_group_member_view"), data=data)
+        self.assertEqual(response.status_code, 200)
+
+        # verify user is not in group anymore
+        response = self.client.get(
+            reverse("splittime:api_group_details_view"), data={"groupId": self.group1.id}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, self.member.username)
+
+    def test_delete_group_member_as_member(self):
+        # login as member
+        self.login(self.member)
+
+        # test begins
+        data = {"user_id": self.creator.id, "group_id": self.group1.id}
+        response = self.client.post(reverse("splittime:api_delete_group_member_view"), data=data)
+        self.assertEqual(response.status_code, 200)
+
+        # verify user is not in group anymore
+        response = self.client.get(
+            reverse("splittime:api_group_details_view"), data={"groupId": self.group1.id}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, self.creator.username)
+
+    def test_delete_group_member_self(self):
+        # login
+        self.login(self.member)
+
+        # test begins
+        data = {"user_id": self.member.id, "group_id": self.group1.id}
+        response = self.client.post(reverse("splittime:api_delete_group_member_view"), data=data)
+        self.assertEqual(response.status_code, 403)
+
+    def test_delete_group_member_as_outsider(self):
+        # login
+        self.login(self.outsider)
+
+        # test begins
+        data = {"user_id": self.member.id, "group_id": self.group1.id}
+        response = self.client.post(reverse("splittime:api_delete_group_member_view"), data=data)
+        self.assertEqual(response.status_code, 403)
