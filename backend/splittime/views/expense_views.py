fe1dcf86018b379b00e9292ee5fde1783598b182
radu <radu.patulescu@gmail.com>
2024-11-17 08:41:57 +0900

    Clean-up Django views implementation

--- backend/splittime/views/expense_views.py
+++ backend/splittime/views/expense_views.py
@@ -1,16 +1,6 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.core.exceptions import PermissionDenied
-from django.http import (
-    HttpResponseForbidden,
-    HttpResponseRedirect,
-    HttpResponseBadRequest,
-    HttpResponseServerError,
-)
 from django.shortcuts import get_object_or_404
-from django.urls import reverse
 from django.utils import timezone
-from django.views import generic
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -20,51 +10,6 @@ from rest_framework import status
 from ..models import Group, Expense
 from ..services.expenses import ExpenseService
 from ..serializers import ExpenseSerializer
-
-
-class ExpenseDetailsView(generic.DetailView):
-    model = Expense
-    template_name = "splittime/expense_details.html"
-
-
-@login_required
-def add_expense(request, group_id):
-    group = get_object_or_404(Group, pk=group_id)
-    payee = get_object_or_404(User, pk=request.POST["payee"])
-
-    if not group.has_member(request.user):
-        return HttpResponseForbidden()
-    try:
-        # Add an expense with the data from the POST request
-        expense = {
-            "name": request.POST["expense_name"],
-            "currency": request.POST["expense_currency"],
-            "amount": request.POST["expense_amount"],
-            "creation_date": timezone.now(),
-            "group": group,
-            "payee": payee,
-        }
-        ExpenseService.add_expense(expense)
-    except Exception as exception:
-        print(exception)
-        return HttpResponseBadRequest(exception)
-
-    return HttpResponseRedirect(reverse("splittime:group_details", args=(group_id,)))
-
-
-@login_required
-def delete_expense(request, pk):
-    expense = get_object_or_404(Expense, pk=pk)
-
-    if not expense.group.has_member(request.user):
-        return HttpResponseForbidden()
-    try:
-        ExpenseService.delete_expense(expense, request.user)
-    except PermissionDenied as exception:
-        print(exception)
-        return HttpResponseServerError(exception)
-
-    return HttpResponseRedirect(reverse("splittime:group_details", args=(expense.group.id,)))
 
 
 class AddExpenseAPIView(APIView):
--- backend/splittime/views/group_views.py
+++ backend/splittime/views/group_views.py
@@ -1,164 +1,21 @@
-from typing import Any
 from datetime import datetime, timedelta
 
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import (
-    HttpResponseRedirect,
-    HttpResponseServerError,
-    HttpResponseForbidden,
-    HttpResponseNotFound,
-)
 from django.shortcuts import get_object_or_404
-from django.urls import reverse
 from django.utils import timezone
-from django.views import generic
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
 
-from ..models import Group, GroupMembership, Expense
+from ..models import Group, GroupMembership
 from ..serializers import GroupSerializer, GroupDetailsSerializer, UserSerializer
-from ..services.balances import BalanceCalculator
 from ..services.groups import GroupService
 from ..exceptions import DuplicateEntryException
 
 
-class IndexView(LoginRequiredMixin, generic.ListView):
-    template_name = "splittime/index.html"
-    context_object_name = "latest_group_list"
-
-    def get_queryset(self):
-        """Returns empy as everything is populated by get_context_data"""
-        return []
-
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        creation_date = timezone.make_aware(
-            datetime.now() - timedelta(days=365), timezone.get_current_timezone()
-        )
-        group_memberships = GroupMembership.objects.filter(member=self.request.user)
-        # latest_group_list = Group.objects.filter(creator=self.request.user).filter(
-        #   creation_date__gte=creation_date).order_by("-creation_date")[:5]
-        latest_group_list = [
-            gm.group for gm in group_memberships if gm.group.creation_date >= creation_date
-        ]
-        latest_group_list.sort(key=lambda x: x.creation_date, reverse=True)
-        context = {"latest_group_list": latest_group_list}
-        return context
-
-
-class GroupDetailsView(LoginRequiredMixin, UserPassesTestMixin, generic.DetailView):
-    model = Group
-    template_name = "splittime/group_details.html"
-
-    def test_func(self):
-        try:
-            group = Group.objects.get(pk=self.kwargs["pk"])
-        except ObjectDoesNotExist as e:
-            return HttpResponseNotFound(e)
-        return group.has_member(self.request.user)
-
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super(GroupDetailsView, self).get_context_data(**kwargs)
-        group = Group.objects.get(pk=self.kwargs["pk"])
-        group_memberships = GroupMembership.objects.filter(group=group)
-
-        expenses = Expense.objects.filter(group=group).order_by("creation_date")
-
-        totals = {}
-        for e in expenses:
-            if e.currency not in totals:
-                totals[e.currency] = e.amount
-                continue
-            totals[e.currency] += e.amount
-
-        balances = BalanceCalculator.calculateBalances(group)
-
-        context = {
-            "group": group,
-            "group_members": group_memberships,
-            "expenses": expenses,
-            "totals": totals,
-            "balances": balances,
-        }
-        return context
-
-
-@login_required
-def add_group(request):
-    if request.method == "POST":
-        group_data = {
-            "name": request.POST["group_name"],
-            "description": request.POST["group_description"],
-            "creator": request.user,
-        }
-        group = Group()
-        try:
-            group = GroupService.add_group(group_data)
-        except Exception as e:
-            return HttpResponseServerError(e)
-        return HttpResponseRedirect(reverse("splittime:group_details", args=(group.id,)))
-    return HttpResponseRedirect(
-        reverse(
-            "splittime:index",
-        )
-    )
-
-
-@login_required
-def delete_group(request, pk):
-    if request.method == "POST":
-        group = get_object_or_404(Group, pk=pk)
-
-        if group.creator.id != request.user.id:
-            return HttpResponseForbidden()
-
-        try:
-            GroupService.delete_group(group)
-        except Exception as e:
-            return HttpResponseServerError(e)
-    return HttpResponseRedirect(reverse("splittime:index"))
-
-
-@login_required
-def add_group_member(request, group_id):
-    user = get_object_or_404(User, email=request.POST["member_email"])
-    group = get_object_or_404(Group, pk=group_id)
-
-    if not group.has_member(request.user):
-        return HttpResponseForbidden()
-    try:
-        GroupService.add_group_member(group, user)
-    except Exception as e:
-        if type(e) is DuplicateEntryException:
-            # messages.add_message(request, messages.INFO, "Member already added")
-            return HttpResponseRedirect(reverse("splittime:group_details", args=(group.id,)))
-        return HttpResponseServerError(e)
-    return HttpResponseRedirect(reverse("splittime:group_details", args=(group.id,)))
-
-
-@login_required
-def delete_group_member(request, group_id, user_id):
-    user = get_object_or_404(User, pk=user_id)
-    group = get_object_or_404(Group, pk=group_id)
-    gm = get_object_or_404(GroupMembership, member=user, group=group)
-
-    if not group.has_member(request.user):
-        return HttpResponseForbidden()
-    try:
-        GroupService.delete_group_member(gm)
-    except Exception as e:
-        return HttpResponseServerError(e)
-
-    return HttpResponseRedirect(reverse("splittime:group_details", args=(group.id,)))
-
-
-# Django Rest Framework views
 class GroupIndexView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -180,7 +37,6 @@ class GroupIndexView(APIView):
         return Response(serializer.data)
 
 
-# Django Rest Framework views
 class AddGroupView(APIView):
     permission_classes = [IsAuthenticated]
 
