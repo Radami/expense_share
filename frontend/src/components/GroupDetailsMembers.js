6bd1dfea9cb2f76f62863ecca92a8a1fc647ac7f
radu <radu.patulescu@gmail.com>
2024-12-28 08:12:34 +0900

    Clean up members grid and add user button

--- frontend/src/components/GroupDetailsMembers.js
+++ frontend/src/components/GroupDetailsMembers.js
@@ -82,25 +82,26 @@ function GroupDetailsMembers({ group_members, group_id, loginParams }) {
       }
 
     return (
-        <div className="container">
+        <div>
             {members && members.length > 0 ? 
                 members.map((gm) => (
-                <div key={gm.id} className="container">
-                    <div className="row">
-                        <div className="col-1">
-                            <i className="bi bi-person-fill fs-1" />
-                        </div>
-                        
-                        <div className="col-6">
-                            <div className="row"> 
-                                <span className="fs-4">{gm.username}</span>
+                <div key={gm.id}>
+                    <div className="d-flex justify-content-between">
+                        <div className='d-flex justify-content-start'>
+                            <div className="">
+                                <i className="bi bi-person-fill fs-1" />
                             </div>
-                            <div className="row"> 
-                                <span className="fs-6">{gm.email}</span>
+                            
+                            <div className="container">
+                                <div className="row"> 
+                                    <span className="fs-4">{gm.username}</span>
+                                </div>
+                                <div className="row"> 
+                                    <span className="fs-6">{gm.email}</span>
+                                </div>
                             </div>
                         </div>
-                        
-                        <div className="col-1 d-flex justify-content-end">
+                        <div className="col-1 d-flex justify-content-center">
                             <button className="btn-red-circle" onClick={() => {deleteGroupMember(gm.id)}}>
                                 <i className="bi bi-x-circle-fill"></i>
                             </button>
@@ -110,14 +111,14 @@ function GroupDetailsMembers({ group_members, group_id, loginParams }) {
             )): (<p>No members found</p>)}
             <div className="container mt-3">
             <form onSubmit={addGroup}>
-                <div className="row">   
-                        <div className="col-6">
-                            <div className="input-group">
-                                <span className="input-group-text">email</span>
-                                <input className="form-control" placeholder="Enter user's email" type="text" id="member_email" name="member_email" />
-                            </div>
+                <div className="d-flex justify-content-center">   
+                    <div>
+                        <div className="input-group">
+                            <span className="input-group-text">email</span>
+                            <input className="form-control" placeholder="Enter user's email" type="text" id="member_email" name="member_email" />
                         </div>
-                    <button className="btn btn-success col-2" type="submit"><i className="bi bi-person-plus-fill" /><span className="ms-1">Add User</span></button>
+                    </div>
+                    <button className="btn btn-success mx-2" type="submit"><i className="bi bi-person-plus-fill" /><span className="ms-1">Add User</span></button>
                 </div>
             </form>
             </div>
